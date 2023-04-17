5557a0127e2f87fc5d1fb5518effc7797d8b38bf
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-17 12:08:08 +0200

    quete Les Sessions en PHP

--- cart.php
+++ cart.php
@@ -1,7 +1,17 @@
 <?php require 'inc/head.php'; ?>
+<?php
+session_start();
+?>
+
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <ul>
+        <?php foreach($_SESSION['cart'] as $product) : ?>
+            <?php foreach ($product as $name) :?>
+            <li><?= $name?></li>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+        </ul>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php require 'inc/data/products.php'; ?>
 <?php require 'inc/head.php'; ?>
+<?php
+
+session_start();
+
+
+if(isset($_GET['add_to_cart'])) {
+    $product = $_GET['add_to_cart'];
+
+    if (isset($_SESSION)) {
+        $_SESSION['cart'][$product] = array('name' => $catalog[$product]['name']);
+    }
+    var_dump($_SESSION);
+}
+?>
 <section class="cookies container-fluid">
     <div class="row">
         <?php foreach ($catalog as $id => $cookie) { ?>
@@ -9,7 +23,7 @@
                     <figcaption class="caption">
                         <h3><?= $cookie['name']; ?></h3>
                         <p><?= $cookie['description']; ?></p>
-                        <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
+                        <a href="?add_to_cart=<?= $id;?>" class="btn btn-primary">
                             <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
                         </a>
                     </figcaption>
